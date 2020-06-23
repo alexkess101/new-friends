937076c0d87ac36077db7735b489532dfb150933
Alex Kessinger <alex@smartystreets.com>
2020-06-22 23:05:22 -0600

    about us title

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -3,9 +3,21 @@ import styles from "./aboutUs.module.scss";
 const AboutUs = () => {
 	return (
 		<div className={styles.container}>
-			<h1>About Us</h1>
-			<h3></h3>
-
+			<div className={styles.titleContainer}>
+				<h2 className={styles.title}>About Us</h2>
+				<h4 className={styles.subTitle}>
+					Everyone creates the best memories in close friend groups.
+					I'm trying to create a dope friend group from scratch to have the
+					best summer ever.
+				</h4>
+			</div>
+			<div className={styles.imgContainer}>
+				<img src={"/svg/couch-solid.svg"} alt={"svg-img"} className={styles.img}/>
+				<img src={"/svg/expeditedssl-brands.svg"} alt={"svg-img"} className={styles.img}/>
+				<img src={"/svg/glass-cheers-solid.svg"} alt={"svg-img"} className={styles.img}/>
+				<img src={"/svg/handshake-regular.svg"} alt={"svg-img"} className={styles.img}/>
+				<img src={"/svg/umbrella-beach-solid.svg"} alt={"svg-img"} className={styles.img}/>
+			</div>
 		</div>
 	)
 };
