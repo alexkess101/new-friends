243e38d020d496cd252f2f399c885e10c049f6f0
Alex Kessinger <alex@smartystreets.com>
2020-07-06 10:39:55 -0500

    added GA

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,11 @@ export default function Home() {
            <title>Creating friends from scratch</title>
            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-171722391-1"></script>
            <script>
-               window.dataLayer = window.dataLayer || [];
-               function gtag(){dataLayer.push(arguments)}
+               { window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
                gtag('js', new Date());
 
-               gtag('config', 'UA-171722391-1');
+               gtag('config', 'UA-171722391-1');}
            </script>
        </Head>
 
