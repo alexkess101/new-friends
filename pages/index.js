a2ab3ad3b4e7109e00be98a665964c9b1bbded93
Alex Kessinger <alex@smartystreets.com>
2020-07-11 22:03:22 -0600

    update meta

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
            <meta charSet="UTF-8"/>
            <meta
                property="og:image"
-               content="/https://dl.boxcloud.com/api/2.0/internal_files/690264863618/versions/732964775618/representations/png_paged_2048x2048/content/1.png?access_token=1!Hub4Csv4G7un6rtpSyp6AuFyyWuKMhFcWQ2nCsiOUajOJtx4nqSqgIxrRgpHhHhkiB5DDdZreV-zt1uxIp2FyhzztBYeC0gKJaqUJ74rv1UqwoAXhov8y-pgPmyaePtoQdIVBX0LKsCFpPxFEumZtg32cHx5QRz0QKrGZ8qXHblyBtuOKdlODNr7HyUJQO_Lbrq28Qn7jAj8CYM_7egOlqxCQAIHNNpLZX1K8RAUyJD_iKdvqqufdpi4bctknKCT1EbVZg8ChGJzcha9nhpR0HktT-mfuJa_yzO8x2zQHi_KvGdcJt_gfX_NLNY0izKIlu9U0iV0gzjipJDHP_mMOc8DgvRh-n_J8VnxMMYDmGaffwRyHPq3w-im23Ey-PdDzovofnP2BXR7J7sNOSS8wORMO9rJdNGrcntDefNqUH20GEP7705k3xhtAnLvYe0e_VJlnpoLYh7N8szdYXMbi-YFYTl_lGJgeEi5JbNleXogXsdK3VFKXbM9wfYwbBwGuxWjwEyH4LSfUmEBJRp3Zs2amJH5iIZKFIz-BDv0Qn9qzPrTRgba3MWkfy8IQEtVFR3LAiR3Aoe-twljCjCVIOh4X2Ydy8oCL_-GYNLj8LjRVtCX6XmRRYiyxhbQFnJIlEQ8QK6bPkZADn2XlyxOBrGe_Ci0xF_tnaJPavTbKDsxf5gA&box_client_name=box-content-preview&box_client_version=2.46.0"
+               content="https://images.unsplash.com/photo-1506869640319-fe1a24fd76dc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80"
            />
        </Head>
 
