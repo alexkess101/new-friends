58f1a756d274d8c51387fe0dcc126a4744163ae6
Alex Kessinger <alex@smartystreets.com>
2020-07-16 17:46:59 -0600

    tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
 					async
 					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
 				/>
+				<script src={"../google-tag-manager.js"} defer/>
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
@@ -23,15 +24,7 @@ export default class MyDocument extends Document {
 						  `,
 					}}
 				/>
-				<script
-					dangerouslySetInnerHTML={{
-						__html:`
-							(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-NLB6CZL');`}}
-				/>
+
 			</Head>
 			<body>
 				<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLB6CZL"
